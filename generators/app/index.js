41a7b57358cb85d073a4265349cf051816ae3206
Danang Nur <danang.arbansa@personio.de>
2020-07-07 21:54:12 +0200

    Add option to choose package manager

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,48 +1,69 @@
-const Generator = require('yeoman-generator');
+const Generator = require("yeoman-generator");
 
 module.exports = class extends Generator {
     async prompting() {
-        this.props = await this.prompt([
+        let props = await this.prompt([
             {
-                name: 'projectName',
-                type: 'input',
+                name: "projectName",
+                type: "input",
                 message: "What's the name of your project?",
                 validate: (page) => {
-                    const isValid = typeof page === 'string' && page && page.length >= 3;
+                    const isValid = typeof page === "string" && page && page.length >= 3;
                     if (isValid) return true;
-                    return 'Your project names should be minimum of 3 characters long!';
+                    return "Your project names should be minimum of 3 characters long!";
                 },
-            }
+            },
+            {
+                name: "shouldInstallDependency",
+                type: "confirm",
+                message: "Do you want to install the dependency?",
+            },
+        ]);
+
+        if (props.shouldInstallDependency) {
+            const newProps = await this.prompt([
+                {
+                    name: "packageManager",
+                    type: "rawlist",
+                    message: "Choose your package manager",
+                    choices: ["npm", "yarn"],
+                },
             ]);
+
+            props = {
+                ...props,
+                ...newProps,
+            };
+        }
+
+        this.props = props;
     }
 
     writing() {
         const { projectName } = this.props;
 
-        this.fs.copy(
-            this.templatePath('./*'),
-            this.destinationPath('', projectName),
-        );
+        this.fs.copy(this.templatePath("./*"), this.destinationPath("", projectName));
 
-        this.fs.copy(
-            this.templatePath('./src'),
-            this.destinationPath('',`${projectName}/src`),
-        );
+        this.fs.copy(this.templatePath("./src"), this.destinationPath("", `${projectName}/src`));
     }
 
     install() {
+        const { packageManager, shouldInstallDependency } = this.props;
+
         this.destinationRoot(`${this.props.projectName}`);
 
-        this.spawnCommandSync('git', ['init', '--quiet']);
+        this.spawnCommandSync("git", ["init", "--quiet"]);
 
-        this.installDependencies({
-            npm: false,
-            bower: false,
-            yarn: true,
-        });
+        if (shouldInstallDependency) {
+            this.installDependencies({
+                npm: packageManager === "npm",
+                bower: false,
+                yarn: packageManager === "yarn",
+            });
+        }
     }
 
     end() {
-        this.log('Let\'s roll! 🤟');
+        this.log("Thanks for using Let's roll! 🤟");
     }
 };
